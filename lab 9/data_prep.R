4d925623b1406e12e3fd7afc9253f7684b957a2f
Huang.Y.Y <105033827+CelesY@users.noreply.github.com>
2024-01-24 16:41:14 +0000

    Delete lab 9 directory

--- lab 9/data_prep.R
+++
@@ -1,8 +0,0 @@
-#data prep-------------
-library(tidyverse)
-library(readr)
-
-df <- read_delim("data/WID_Data_30112023-093225.csv", delim = ";", escape_double = FALSE, trim_ws = TRUE, skip = 1)%>%
-  rename(percentile='Percentile',
-         date='Year',
-         value='sptinc_z_ES\nPre-tax national income \nTop 10% | share\nSpain')
